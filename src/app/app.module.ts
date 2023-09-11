88d559f06dc9ba47cc5c4e08d2e503b818474a7d
Daniela Rios <daniela.rios.v91@gmail.com>
2023-09-11 17:43:42 -0300

    module modify directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { SharedModule } from './shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { LayoutComponent } from './auth/pages/layout/layout.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutHeroeComponent } from './heroes/pages/layout-heroe/layout-heroe.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    //LayoutComponent
+    LayoutHeroeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutComponent } from '../auth/pages/layout/layout.component';
 import { NewComponent } from './pages/new/new.component';
 import { SearchComponent } from './pages/search/search.component';
 import { ListComponent } from './pages/list/list.component';
 import { HeroComponent } from './pages/hero/hero.component';
+import { LayoutHeroeComponent } from './pages/layout-heroe/layout-heroe.component';
+
 
 const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
+    component: LayoutHeroeComponent,
     children: [
       { path: 'new', component: NewComponent },
       { path: 'search', component: SearchComponent },
