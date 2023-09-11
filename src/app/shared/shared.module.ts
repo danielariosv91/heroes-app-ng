a70ad8e22310764939ddf892fe1013ce01257438
Daniela Rios <daniela.rios.v91@gmail.com>
2023-09-11 12:29:12 -0300

    creates pages

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Error404Component } from './pages/error404/error404.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Error404Component
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    Error404Component
   ]
 })
 export class SharedModule { }
