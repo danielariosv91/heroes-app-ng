a0fe70ff9e21ae763e0d743c08b87d1dbdd8c6b5
Daniela Rios <daniela.rios.v91@gmail.com>
2023-09-11 15:00:58 -0300

    create login and register views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent, 
-    LayoutComponent
+    //LayoutComponent
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth.routing.module';
+import { MaterialModule } from '../material/material.module';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
