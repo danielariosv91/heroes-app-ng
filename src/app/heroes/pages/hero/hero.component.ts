a28c141acf2db95399a67f41b3af438e761eef94
Daniela Rios <daniela.rios.v91@gmail.com>
2023-09-12 21:50:52 -0300

    deatil url hero

--- src/app/heroes/pages/hero/hero.component.ts
+++ src/app/heroes/pages/hero/hero.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../../services/hero.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero',
@@ -6,6 +10,25 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
 
+  public hero?: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.heroService.getHeroesById(id))
+      ).subscribe(hero => {
+        if (!hero) return this.router.navigate(['/heroe/list'])
+
+        this.hero = hero;
+        return;
+      })
+  }
 }
--- src/app/heroes/services/hero.service.ts
+++ src/app/heroes/services/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
@@ -13,5 +13,13 @@ export class HeroService {
 
     getHeroes(): Observable<Hero[]> {
         return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+    }
+
+    getHeroesById(id: string): Observable<Hero | undefined> {
+        return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+            .pipe(
+                /** if there is an error, return a undefined as obs */
+                catchError(error => of(undefined))
+            )
     }
 }
