a8fd73ac3a63b2c14d506517e9841d47854de53e
Daniela Rios <daniela.rios.v91@gmail.com>
2023-09-28 16:27:33 -0300

    search component

--- src/app/heroes/services/hero.service.ts
+++ src/app/heroes/services/hero.service.ts
@@ -24,6 +24,6 @@ export class HeroService {
     }
 
     getSuggestions(query: string): Observable<Hero[]> {
-        return this.http.get<Hero[]>(`/heroes?q=${query}`)
+        return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}`)
     }
 }
